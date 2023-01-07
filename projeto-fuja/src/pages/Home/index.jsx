68db5a5b363555656275a6ac31fec1c69b8223a3
ademaneto01 <ademarneto47@gmail.com>
2023-01-07 19:03:16 -0300

    populando pagina home

--- projeto-fuja/src/pages/Home/index.jsx
+++ projeto-fuja/src/pages/Home/index.jsx
@@ -5,7 +5,36 @@ function App() {
     <div className="container">
       <Header />
       <div className="mainVideo">
-        <h1>RUN</h1>
+        <h1>Sua produção audiovisual fora do convencional</h1>
+      </div>
+      <div className="containerCarrousel">
+        <div className="infosCarrousel">
+          <div className="textCarrousel">
+            <h1>
+              O Fuja é um estilo de vida.<br></br>Uma inspiração.<br></br> É
+              liberdade.
+            </h1>
+          </div>
+          <div className="boxImgCarrousel"></div>
+        </div>
+      </div>
+      <div className="screamVideosFuja">
+        <div className="boxPrincipalInfosVideos">
+          <h1 className="titleVideos">
+            O Fuja é jovem, mas já trabalhou e já teve o seu <br></br> conteúdo
+            veiculado em vários locais
+          </h1>
+          <h2 className="subtileVideos">
+            Seja um vídeo de 5 segundos ou um curta-metragem, a equipe do Fuja
+            possui os melhores<br></br> equipamentos para a produção de um
+            conteúdo objetivo, diferente e engajador.
+          </h2>
+          <div className="boxVideos">
+            <div className="video"></div>
+            <div className="video"></div>
+            <div className="video"></div>
+          </div>
+        </div>
       </div>
     </div>
   );
