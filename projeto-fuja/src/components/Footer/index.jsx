461821a1e4a6a585b755b085d5f69559f951b7ab
Bruno Luís Bahiense <brunoluisbahiense@gmail.com>
2023-01-12 19:48:05 -0300

    footer updated

--- projeto-fuja/src/components/Footer/index.jsx
+++ projeto-fuja/src/components/Footer/index.jsx
@@ -17,10 +17,10 @@ const Footer = () => (
             <div className="social-media">
                 <p>Acompanhe a gente nas redes sociais</p>
                 <div className='social-media-icons'>
-                    <a  href="https://www.youtube.com/">youtube</a>
-                    <a  href="https://www.instagram.com/">instagram</a>
-                    <a  href="https://www.tiktok.com/">tiktok</a>
-                    <a  href="https://www.facebook.com/">facebook</a>
+                    <a  href="https://www.youtube.com/"><img src="/img/social-icons/youtube.svg" alt="youtube"/></a>
+                    <a  href="https://www.instagram.com/"><img src="/img/social-icons/insta.svg" alt="insta"/></a>
+                    <a  href="https://www.tiktok.com/"><img src="/img/social-icons/tiktok.svg" alt="tiktok"/></a>
+                    <a  href="https://www.facebook.com/"><img src="/img/social-icons/face.svg" alt="face"/></a>
                 </div>
                 <div className="contact">
                     <a href="mailto:fuja@gmail.com">fuja@gmail.com</a> | <a href="tel:5511983743838">+55 (11) 98374-3838</a>
