be9984a9fbc3e11af8fef74ceb9fd1aecb1deb43
Bruno Luís Bahiense <brunoluisbahiense@gmail.com>
2023-02-19 20:02:41 -0300

    pagina de contato da fuja

--- projeto-fuja/src/index.js
+++ projeto-fuja/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Home from './pages/Home';
+//import Home from './pages/Home';
+import Contact from './pages/Contact';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <Contact />
   </React.StrictMode>
 );
 
---
+++ projeto-fuja/src/pages/Contact/index.jsx
@@ -0,0 +1,33 @@
+import "./styles.css";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+
+export default function Contact() {
+  return (
+    <div className="container">
+      <Header />
+      <div className="contactPage">
+        <div className="contactPageInfo">
+          <h1>FUJA DO CONVENCIONAL</h1>
+          <div className="contactList">
+            <h2>Entre em contato</h2>
+            <a href="mailto:fuja@gmail.com">fuja@gmail.com</a>
+            <a href="tel:5511983743838">+55 (11) 98374-3838</a>
+            <div className="social-media">
+                <p>Acompanhe a gente nas redes sociais</p>
+                <div className='social-media-icons'>
+                    <a  href="https://www.youtube.com/"><img src="/img/social-icons/youtube.svg" alt="youtube"/></a>
+                    <a  href="https://www.instagram.com/"><img src="/img/social-icons/insta.svg" alt="insta"/></a>
+                    <a  href="https://www.tiktok.com/"><img src="/img/social-icons/tiktok.svg" alt="tiktok"/></a>
+                    <a  href="https://www.facebook.com/"><img src="/img/social-icons/face.svg" alt="face"/></a>
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+
