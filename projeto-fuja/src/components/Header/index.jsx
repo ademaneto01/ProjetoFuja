c4903789c962094acd63aefbc08820bef67e3f19
Thierry Castro <thierrycastro555@gmail.com>
2023-03-31 15:12:23 -0300

    validação de cor dos botões header

--- projeto-fuja/src/components/Header/index.jsx
+++ projeto-fuja/src/components/Header/index.jsx
@@ -1,77 +1,121 @@
 import "./styles.css";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Modal from '@mui/material/Modal';
-
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
 
 const Header = () => {
+  const location = useLocation();
+  const isPortfolioRoute = location.pathname === "/portfolio";
+
   const navigate = useNavigate();
   const [open, setOpen] = React.useState(false);
-  
-  const handleOpen = (() => {
-    setOpen(true)
 
-  });
+  const handleOpen = () => {
+    setOpen(true);
+  };
 
-  const handleClose = (() => {
-    setOpen(false)
-    
-  });
-
-
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   function BasicModal() {
+    return (
+      <div>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box id="modalHeader">
+            <div className="boxBtnHeader">
+              <div className="btnCloseModalHeader" onClick={handleClose}>
+                <div className="btnSvg"></div>
+              </div>
+            </div>
+            <div className="boxNavModal">
+              <div
+                className="titleModalHeader"
+                onClick={() => {
+                  navigate("/");
+                }}
+              >
+                Home
+              </div>
+              <div
+                className="titleModalHeader"
+                onClick={() => {
+                  navigate("/portfolio");
+                }}
+              >
+                Portfólio
+              </div>
+              <div className="titleModalHeader">Portal de aventuras</div>
+              <div
+                className="titleModalHeader"
+                onClick={() => {
+                  navigate("/contact");
+                }}
+              >
+                Contrate o Fuja
+              </div>
+            </div>
+            <div className="containerFooterModal">
+              <div className="logoHeader"> </div>
+              <div className="textModalFooter">
+                A sua produção audiovisual fora do convencional
+              </div>
+            </div>
+          </Box>
+        </Modal>
+      </div>
+    );
+  }
 
   return (
-    <div>
-   
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box id="modalHeader">
-          <div className="boxBtnHeader">
-          <div className="btnCloseModalHeader" onClick={handleClose}>
-            <div className="btnSvg"></div>
-          </div>
-          </div>
-          <div className="boxNavModal">
-            <div className="titleModalHeader"  onClick={()=> {navigate("/")}}>Home</div>
-            <div className="titleModalHeader"  onClick={()=> {navigate("/portfolio")}}>Portfólio</div>
-            <div className="titleModalHeader">Portal de aventuras</div>
-            <div className="titleModalHeader"  onClick={()=> {navigate("/contact")}}>Contrate o Fuja</div>
-          </div>
-          <div className='containerFooterModal'>
-                <div className="logoHeader"> </div>
-            <div className="textModalFooter">A sua produção audiovisual fora do convencional</div>
-        </div>
-        </Box>
-      </Modal>
-    </div>
-  );
-}
-
-  return(
     <>
-  <header className="header" style={{position: open ? 'absolute' : 'fixed'}} >
-    <nav onClick={handleOpen}>
-     <img src="/img/menu.png" alt="simbolo de menu" className="menu-icon" />
-    </nav>
+      <header
+        className="header"
+        style={{ position: open ? "absolute" : "fixed" }}
+      >
+        <nav
+          className={isPortfolioRoute ? "buttonsPortfolio" : "buttonsHome"}
+          onClick={handleOpen}
+        >
+          <img
+            src="/img/menu.png"
+            alt="simbolo de menu"
+            className="menu-icon"
+          />
+        </nav>
 
-    
-      <img src="/img/logoFuja.svg" onClick={()=> {navigate("/")}} alt="logoFuja" className="logo-icon" />
-   
+        <img
+          src="/img/logoFuja.svg"
+          onClick={() => {
+            navigate("/");
+          }}
+          alt="logoFuja"
+          className={
+            isPortfolioRoute
+              ? "logo-icon buttonsPortfolio"
+              : "logo-icon buttonsHome"
+          }
+        />
 
-    <button onClick={() => {navigate("/contact")}}>Contrate o Fuja</button>
-   
-  </header>
-  <BasicModal/>
-  </>
+        <button
+          className={isPortfolioRoute ? "buttonsPortfolio" : "buttonsHome"}
+          onClick={() => {
+            navigate("/contact");
+          }}
+        >
+          Contrate o Fuja
+        </button>
+      </header>
+      <BasicModal />
+    </>
   );
-}
+};
 
 export default Header;
